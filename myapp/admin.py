811c650546feff043513006f14eeffef622316c5
LukPet <L.Petrusek@seznam.cz>
2016-09-07 07:35:33 +0200

    pzn

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from .models import Uloziste
-
-
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
-
+import sys
 
 def make_active(modeladmin, news, queryset):
     queryset.update(is_active=True)
@@ -19,11 +17,27 @@ admin.site.register(User, CustomUserAdmin)
 
 admin.site.site_header = 'Správa pacientů'
 
+def Pozn(obj):
+    return obj.pzn!=""
+Pozn.boolean = True
+
 class adminUloziste(admin.ModelAdmin):
-    list_display = ('name', 'birthNumber', 'diagnosis', 'radAs', 'doctor', 'resimulTime', 'drawingTime', 'drawingDoctor', 'planningTime', 'planningDoctor', 'ok1', 'ok2', 'tisk')
+    fieldsets = (
+        (None, {
+            'fields': (('name', 'birthNumber'), ('diagnosis', 'radAs'), 'doctor', 'resimulTime', ('drawingTime', 'drawingDoctor'), ('planningTime', 'planningDoctor'), ('ok1', 'ok2'), ('pomucky', 'tisk'), 'pzn')
+        }),
+       )
+
+    list_display = ('id', 'name', 'birthNumber', 'diagnosis', 'radAs', 'doctor', 'resimulTime', 'drawingTime', 'drawingDoctor', 'planningTime', 'planningDoctor', 'ok1', 'ok2', 'pomucky', 'tisk', Pozn)
+
+    #readonly_fields = ('pznDisplay',)
+    #list_display = ('name', 'birthNumber', 'diagnosis', 'radAs', 'doctor', 'resimulTime', 'drawingTime', 'drawingDoctor', 'planningTime', 'planningDoctor', 'ok1', 'ok2', 'pomucky', 'tisk', 'pznDisplay')
     #list_filter = ['name', 'birthNumber', 'diagnosis', 'radAs', 'doctor', 'resimulTime', 'drawingTime', 'drawingDoctor', 'planningTime', 'planningDoctor', 'ok1', 'ok2', 'tisk']
-    search_fields = ['name', 'birthNumber', 'diagnosis', 'radAs', 'doctor', 'resimulTime', 'drawingTime', 'drawingDoctor', 'planningTime', 'planningDoctor', 'ok1', 'ok2', 'tisk']
+    search_fields = ['name', 'birthNumber', 'diagnosis', 'radAs', 'doctor', 'resimulTime', 'drawingTime', 'drawingDoctor', 'planningTime', 'planningDoctor', 'ok1', 'ok2', 'pomucky', 'tisk', 'pzn']
     #date_hierarchy = 'resimulTime'
     list_per_page = 50
 
+
+
 admin.site.register(Uloziste, adminUloziste)
+
--- myapp/models.py
+++ myapp/models.py
@@ -9,28 +9,41 @@ from datetime import timedelta
 # Create your models here.
 
 class Uloziste(models.Model):
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    DOCTOR = (
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+    )
+
     name = models.CharField('Jméno', max_length=100)
     birthNumber = models.CharField('Rodné číslo', max_length=10)
     diagnosis = models.CharField('Diagnóza', max_length=100, blank=True)
     radAs = models.CharField('Rad. As.', max_length=100, blank=True)
-    doctor = models.CharField('Lékař', max_length=100, blank=True)
-    resimulTime = models.DateTimeField('Datum a čas resimulace', default=timezone.now, blank=True)
-    drawingTime = models.DateTimeField('Datum zakreslení', default=timezone.now, blank=True)
-    drawingDoctor = models.CharField('Zakreslení stav/lékař', max_length=100, blank=True)
-    planningTime = models.DateTimeField('Datum plánování', default=timezone.now, blank=True)
-    planningDoctor = models.CharField('Plánování stav/plánovač', max_length=100, blank=True)
-    ok1 = models.CharField('Schválení lékař 1', max_length=100, blank=True)
-    ok2 = models.CharField('Schválení lékař 2', max_length=100, blank=True)
+    doctor = models.CharField(max_length=2, choices=DOCTOR, default=FRESHMAN,)
+    resimulTime = models.DateTimeField('Resimulace', default=timezone.now, blank=True)
+    drawingTime = models.DateField('Zakreslení', default=timezone.now, blank=True)
+    drawingDoctor = models.CharField('Zakreslení stav', max_length=100, blank=True)
+    planningTime = models.DateField('Plánování', default=timezone.now, blank=True)
+    planningDoctor = models.CharField('Plánování stav', max_length=100, blank=True)
+    ok1 = models.CharField('Schválení 1', max_length=100, blank=True)
+    ok2 = models.CharField('Schválení 2', max_length=100, blank=True)
+    pomucky = models.CharField('Pomůcky', max_length=100, blank=True)
     tisk = models.BooleanField(default=True)
-    pzn = models.TextField('Poznámka', default='',blank=True)
+    pzn = models.TextField('Poznámka', blank=True,)
+    pznDisplay = models.BooleanField('Poznámka', default=False)
 
     class Meta:
         verbose_name = 'Pacient'
         verbose_name_plural = 'Pacienti'
-
 
     def __str__(self):
         return self.name
 
     def __unicode__(self):
         return self.name
+
